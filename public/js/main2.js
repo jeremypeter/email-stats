578fe5bf788351417c226bdd380e880f84bc1810
jeremypeter <jeremy.peter@meltmedia.com>
2014-08-14 21:49:23 -0700

    adding drilldown functionality

--- public/js/main2.js
+++ public/js/main2.js
@@ -23,59 +23,142 @@ $(function () {
 
   });
 
-var haha = [];
+  var clientData = [];
+  var versionData = [];
+  var clients;
+  var versions;
+
+  var others;
+  var othersTotal = 0;
+  var otherVersions = {};
+  otherVersions.id = 'Others';
+  otherVersions.data = [];
+
   _.each(dataObj, function(clientObj, clientName){
-      var count = 0;
-      var obj = {};
-      obj.name = clientName
-      obj.y = 0
+    
+    var versionLength = Object.keys(clientObj).length;
+
+    // Create an empty object for that will hold the name and y value
+    // Set the name of email client and y value to start at 0;
+    clients = {};
+    clients.name = clientName
+    clients.y = 0
+
+    // If there is more than one version of an email client, let's 
+    // drill down to those specific versions
+    if(versionLength > 1 ) {  clients.drilldown = clientName };
+
+    // Create object that will be filled with the different client versions
+    versions = {};
+    versions.id = clientName;
+    versions.data = [];
+
+
+    // Loop through each client version and add the totals 
     _.each(clientObj, function(versionObj, versionName){
 
-      obj.y += clientObj[versionName].value ;
+      // Add the total value of all versions of email client
+      clients.y += versionObj.value;
 
-      dataArr.push({
-        name: versionName,
-        y: +Number(versionObj.value/total * 100).toFixed(2)
+      // Create drilldown data
+      versions.data.push({ name: versionName, y: versionObj.value })
+
+    });
+
+    // Create others category
+    var average = Number(clients.y/total * 100);
+    if(average < 2){
+
+      others = {};
+      others.name = 'Others';
+      others.y = othersTotal += clients.y
+      others.drilldown = 'Others';
+
+      otherVersions.data.push({ name: clients.name, y: clients.y });
+
+      versionData.push(otherVersions);
+
+    }else{
+      clientData.push(clients);
+      versionData.push(versions)
+    }
+    
+  });
+  
+  clientData.push(others)
+
+  function getClientData(dataObj){
+
+    var clientData = [];
+    var clients;
+
+    _.each(dataObj, function(clientObj, clientName){
+     
+      clients = {};
+      clients.name = clientName;
+      clients.y = 0;
+      clients.versionLength = Object.keys(clientObj).length;
+
+      _.each(clientObj, function(versionObj, versionName){
+        clients.y += versionObj.value;
       });
 
-    });
-      haha.push(obj)
+      clientData.push(clients);
 
-  });
-  console.log(haha);
-// console.log(dataObj);
-// console.log(testobj);
-// console.log(dataArr);
-
-    // Build the chart
-    $('#container').highcharts({
-        chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false
-        },
-        title: {
-            text: 'Browser market shares at a specific website, 2014'
-        },
-        tooltip: {
-            pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-        },
-        plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: false
-                },
-                showInLegend: true
-            }
-        },
-        series: [{
-            type: 'pie',
-            name: 'Email Market share',
-            data: haha
-        }
-        ]
     });
+
+    return clientData;
+  }
+
+  function getVersionData(dataObj){
+    
+  }
+
+  console.log(getClientData(dataObj));
+
+
+  // console.log(clientData);
+  // console.log(versionData);
+
+  var config = {
+      chart: {
+          type: 'pie',
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false
+      },
+      title: {
+          text: 'Browser market shares at a specific website, 2014'
+      },
+      tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        borderWidth: 1
+      },
+      plotOptions: {
+          pie: {
+              // allowPointSelect: true,
+              cursor: 'pointer',
+              dataLabels: {
+                  enabled: false
+              },
+              showInLegend: true
+          }
+      },
+      series: [{
+          type: 'pie',
+          name: 'Email Market share',
+          data: clientData
+      }],
+      drilldown: {
+        series: versionData
+      }
+  }
+
+  // Build the chart
+  $('#container').highcharts(config);
 
 });
